1ec1f246ba11dd29885ce8a6f93f7506cf79b209
simplonfl <fabrice-laroche81@live.fr>
2017-11-16 15:58:30 +0100

    // Exercice

--- array/app.js
+++ array/app.js
@@ -1,1 +1,23 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+// Exercice 1	
+
+	var mois = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre'];
+
+// Exercice 2
+	
+	console.log(mois[2]);
+
+// Exercice 3
+
+	console.log(mois[5]);
+
+// Exercice 4
+
+	console.log(mois[10]);
+
+
+
+
+
+
